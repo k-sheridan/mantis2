6eae9815e2badc99a3b1ecc22fbf821488c8d5f8
Trexter <sheridak@purdue.edu>
2017-07-19 01:59:55 -0400

    started markov model

--- include/mantis2/XYMarkovModel.cpp
+++ include/mantis2/XYMarkovModel.cpp
@@ -11,8 +11,12 @@ XYMarkovModel::XYMarkovModel() {
 	int cols = ((GRID_WIDTH * GRID_SPACING) / (double)XY_MARKOV_RESOLUTION) + (XY_MARKOV_EDGE_PADDING);
 	int rows = ((GRID_HEIGHT * GRID_SPACING) / (double)XY_MARKOV_RESOLUTION) + (XY_MARKOV_EDGE_PADDING);
 
-	this->P = cv::Mat(rows, cols, CV_64F); // create the model
+	ROS_DEBUG_STREAM("xy model size: " << rows*cols);
 
+	this->P = cv::Mat(rows, cols, CV_64F); // create the model
+	this->makeUniform();
+	this->computeEntropy();
+	ROS_DEBUG_STREAM("uniform entropy: " << this->entropy);
 }
 
 XYMarkovModel::~XYMarkovModel() {
@@ -20,10 +24,31 @@ XYMarkovModel::~XYMarkovModel() {
 }
 
 void XYMarkovModel::makeUniform(){
+	int size = this->P.rows * this->P.cols;
 
+	double probability = 1 / (double)size;
+	for(int i = 0; i < size; i++)
+	{
+		this->P(i) = probability;
+	}
 }
 
 double XYMarkovModel::computeEntropy(){
+	double sum = 0;
+	double sumLog = 0;
 
+	int size = this->P.rows * this->P.cols;
+	for(int i = 0; i < size; i++)
+	{
+		sum += this->P(i);
+	}
+
+	for(int i = 0; i < size; i++)
+	{
+		sumLog += log(this->P(i));
+	}
+
+	this->entropy = -sum*sumLog;
+	return this->entropy;
 }
 
